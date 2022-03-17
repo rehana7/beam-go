936935efd548f41bc2902bb72f8dcf5c4f103fcd
rehana7 <89412687+rehana7@users.noreply.github.com>
2022-03-17 08:34:48 -0500

    adding the go files.

---
+++ hello.go
@@ -0,0 +1,13 @@
+package main
+
+// Import the popular fmt package,
+// which contains functions for formatting text,
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hello, 世界!")
+	fmt.Println("Hello, వరల్డ్!")
+	fmt.Println("Hello, world!")
+}
